c9cb409a0027713e61ae3317a24706e174c434ef
Asik <asik.mirikov@mail.ru>
2022-03-24 22:45:07 +0300

    feat: Добавить логику для попапа

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+let editButton = document.querySelector('.profile__edit-button');
+let profileName = document.querySelector('.profile__name-text');
+let profileActivity = document.querySelector('.profile__activity');
+
+let popup = document.querySelector('.popup');
+let formPopup = popup.querySelector('.popupForm');
+let closeButton = popup.querySelector('.popup__close-button');
+let nameInput = popup.querySelector('#name');
+let jobInput = popup.querySelector('#work');
+let submitButton = popup.querySelector('.popup__button');
+
+function openPopup () {
+  popup.classList.add('popup_opened');
+
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileActivity.textContent;
+}
+
+function closePopup () {
+  popup.classList.remove('popup_opened');
+}
+
+function submitPopup () {
+  profileName.textContent = nameInput.value;
+  profileActivity.textContent = jobInput.value;
+
+  closePopup();
+}
+
+submitButton.addEventListener('click', submitPopup);
+closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', openPopup);
