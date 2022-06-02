e4bf2d37c17c60e4fa9b93ce82775a9996b87832
Asik <asik.mirikov@mail.ru>
2022-06-02 21:35:19 +0300

    feat: Добавить API. Получить инфо о юзере

---
+++ src/components/Api.js
@@ -0,0 +1,48 @@
+export default class Api {
+  constructor(data) {
+    this._baseUrl = data.baseUrl;
+    this._headers = data.headers;
+  }
+
+  _request = (url, method, body) => {
+    return fetch(this._baseUrl + url, {
+      headers: this._headers,
+      method: method,
+      body: body
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+
+        return Promise.reject(`Ошибка: ${res.status}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  _get = (url) => {
+    return this._request(url, 'GET');
+  };
+
+  _post = (url, body) => {
+    return this._request(url, 'POST', body);
+  };
+
+  _patch = (url, body) => {
+    return this._request(url, 'PATCH', body);
+  };
+
+  _put = (url, body) => {
+    return this._request(url, 'PUT', body);
+  };
+
+  _delete = (url) => {
+    return this._request(url, 'DELETE');
+  };
+
+  getUserInfo = () => {
+    return this._get('/users/me');
+  };
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Api from '../components/Api.js';
 import { Card } from '../components/Card.js';
 import { FormValidator } from '../components/FormValidator.js';
 import Section from '../components/Section.js';
@@ -74,6 +75,14 @@ const cardsSection = new Section({
 
 const user = new UserInfo({nameSelector: '.profile__name-text', infoSelector: '.profile__activity'});
 
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-42',
+  headers: {
+    authorization: 'f7ca521b-6071-408d-9596-93540c9b5ffb',
+    'Content-Type': 'application/json'
+  }
+});
+
 function handleCardClick(imageSrc, titleText) {
   popupPreview.open(imageSrc, titleText);
 }
@@ -83,6 +92,10 @@ function startPage() {
 
   popupPlaceAddFormValidator.enableValidation();
   profilePopupFormValidator.enableValidation();
+
+  api.getUserInfo().then((result) => {
+    user.setUserInfo(result.name, result.about);
+  });
 }
 
 function generateCard(data) {
