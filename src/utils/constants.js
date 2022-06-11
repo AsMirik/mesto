313e296f5e2d8001a8c000319947e68de832f8c9
Asik <asik.mirikov@mail.ru>
2022-06-11 19:04:38 +0300

    fix: Исправить замечания

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+const validationSettings = {
+  fieldSelector: '.popup__field',
+  inputSelector: '.popup__input',
+  errorElementSelector: '.popup__input-error',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_error',
+  errorMessageActiveClass: 'popup__input-error_active',
+};
+
+export { validationSettings };
